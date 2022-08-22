40250db993257a4b21debc02051919268b248c9b
Rajnish Pratap Singh <rps.rajnish@gmail.com>
2022-08-22 16:34:09 +0530

    js bug fixes

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 const billInput = document.querySelector(".billInput");
 const tipInput = document.querySelector(".tipInput");
-const noPeople = document.querySelector(".noPeople");
+const numPeople = document.querySelector(".numPeople");
 const total = document.querySelector(".total");
-const minus = document.querySelector(".noPeople");
 const reset = document.querySelector(".reset");
 
-let peopleo = Number(minus.textContent);
-console.log(peopleo);
+let persons = Number(numPeople.textContent);
+
 const minusPeople = () => {
-  if (minus.textContent > 1) {
-    document.querySelector(".noPeople").textContent -= 1;
+  if (numPeople.textContent > 1) {
+    numPeople.textContent -= 1;
   }
   calculateBill();
 };
 const plusPeople = () => {
-  peopleo = Number(minus.textContent);
-  peopleo += 1;
-  document.querySelector(".noPeople").textContent = peopleo;
+  persons = Number(numPeople.textContent);
+  numPeople.textContent = persons += 1;
 
   calculateBill();
 };
@@ -25,13 +23,13 @@ const calculateBill = () => {
   let bill = Number(billInput.value);
   let tip = Number(tipInput.value);
   let tips = (bill / 100) * tip;
-  let peo = Number(noPeople.textContent);
+  let peo = Number(numPeople.textContent);
   total.textContent = Math.ceil((bill + tips) / peo);
 };
 
 const resetit = () => {
-  minus.textContent = 1;
+  numPeople.textContent = 1;
   billInput.value = "";
   tipInput.value = "";
-  total.textContent = '0';
+  total.textContent = "0";
 };
